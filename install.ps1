b029eeeb11859573189c5b9f97deb8ea9b0b9257
b1n <120295547+b1nhack@users.noreply.github.com>
2024-05-17 03:57:31 +0800

    feat: clean build caches

--- install.ps1
+++ install.ps1
@@ -1,2 +1,3 @@
 cargo build --release
 mv .\target\release\lua_json5.dll lua\json5.dll
+cargo clean
--- install.sh
+++ install.sh
@@ -6,10 +6,12 @@ case $OSTYPE in
 "linux-gnu"*)
 	mv ./target/release/liblua_json5.so lua/json5.so
 	strip lua/json5.so
+	cargo clean
 	;;
 "darwin"*)
 	# Provide both just in case
 	cp ./target/release/liblua_json5.dylib lua/json5.dylib
 	cp lua/json5.dylib lua/json5.so
+	cargo clean
 	;;
 esac
